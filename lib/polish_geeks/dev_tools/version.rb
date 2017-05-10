5b7dd430fe1734f279e30df88a9f522284ccc75a
David Stancu <Stancu@toasterspam.home>
2017-05-10 16:19:15 -0400

    update rules and bump version

--- lib/polish_geeks/dev_tools/version.rb
+++ lib/polish_geeks/dev_tools/version.rb
@@ -3,6 +3,6 @@ module PolishGeeks
   # Dev Tools for PolishGeeks developers
   module DevTools
     # Current version of dev tools
-    VERSION = '1.4.0'.freeze
+    VERSION = '1.4.1'.freeze
   end
 end
